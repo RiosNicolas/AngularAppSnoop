d992ab1f669e032bc94041161d459c031c2c0da8
Nicolas <nicolasgastonrios@gmail.com>
2018-12-09 16:27:21 -0300

    Agregar archivos de proyecto.

---
+++ MvcAngularAppSnoop/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcAngularAppSnoop.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Apellido { get; set; }
+    }
+}
---
+++ MvcAngularAppSnoop/Models/MvcAngularAppSnoopDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MvcAngularAppSnoop.Models
+{
+    public class MvcAngularAppSnoopDbContext : DbContext
+    {
+        public MvcAngularAppSnoopDbContext() : base("name = MvcAngularAppSnoopDbContext")
+        {
+
+        }
+
+        // convención de ajuste
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
